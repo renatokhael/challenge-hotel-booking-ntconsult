0928f2454fda08e17137b35715a25364a3c52325
Renato Khael <renatokhael@gmail.com>
2024-06-27 21:29:22 -0300

    fix: configure json-server to run vercel

---
+++ api/json-server.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+// @ts-nocheck
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json') // Path to your db.json file
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+module.exports = server
